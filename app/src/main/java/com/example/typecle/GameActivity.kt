222c9f63a7082f39c456b5c86c3de2f60596b8c0
ayo <au_salako@hotmail.com>
2020-04-22 21:00:03 +0100

    made game layout

--- app/src/main/java/com/example/typecle/GameActivity.kt
+++ app/src/main/java/com/example/typecle/GameActivity.kt
@@ -2,11 +2,17 @@ package com.example.typecle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
-class GameActivity : AppCompatActivity() {
+abstract class GameActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+
+    abstract fun onCreate();
+    abstract val time: Date;
+    abstract val content: String;
+
 }
